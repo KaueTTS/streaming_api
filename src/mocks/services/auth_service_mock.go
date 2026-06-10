661094fa34222a7ee60da66d090046d519307f11
unknown <kauebertaze2004@gmail.com>
2026-06-09 23:57:25 -0300

    test: implementation of authentication and profile mocks

--- src/mocks/services/auth_service_mock.go
+++ src/mocks/services/auth_service_mock.go
@@ -13,18 +13,15 @@ type AuthServiceMock struct {
 
 func (m *AuthServiceMock) Register(ctx context.Context, request dto_auth.RegisterRequestDto) (dto_auth.UserResponseDto, error) {
 	args := m.Called(ctx, request)
-
 	return args.Get(0).(dto_auth.UserResponseDto), args.Error(1)
 }
 
 func (m *AuthServiceMock) Login(ctx context.Context, request dto_auth.LoginRequestDto) (dto_auth.AuthResponseDto, error) {
 	args := m.Called(ctx, request)
-
 	return args.Get(0).(dto_auth.AuthResponseDto), args.Error(1)
 }
 
 func (m *AuthServiceMock) Me(ctx context.Context, userID uint) (dto_auth.UserResponseDto, error) {
 	args := m.Called(ctx, userID)
-
 	return args.Get(0).(dto_auth.UserResponseDto), args.Error(1)
 }
--- src/mocks/services/profile_service_mock.go
+++ src/mocks/services/profile_service_mock.go
@@ -13,24 +13,20 @@ type ProfileServiceMock struct {
 
 func (m *ProfileServiceMock) ListProfiles(ctx context.Context, userID uint, page, perPage int) (dto_profile.ProfileResponseDto, error) {
 	args := m.Called(ctx, userID, page, perPage)
-
 	return args.Get(0).(dto_profile.ProfileResponseDto), args.Error(1)
 }
 
 func (m *ProfileServiceMock) CreateProfile(ctx context.Context, userID uint, request dto_profile.ProfileRequestDto) (dto_profile.ProfileDto, error) {
 	args := m.Called(ctx, userID, request)
-
 	return args.Get(0).(dto_profile.ProfileDto), args.Error(1)
 }
 
 func (m *ProfileServiceMock) UpdateProfile(ctx context.Context, userID, profileID uint, request dto_profile.ProfileRequestDto) (dto_profile.ProfileDto, error) {
 	args := m.Called(ctx, userID, profileID, request)
-
 	return args.Get(0).(dto_profile.ProfileDto), args.Error(1)
 }
 
 func (m *ProfileServiceMock) DeleteProfile(ctx context.Context, userID, profileID uint) error {
 	args := m.Called(ctx, userID, profileID)
-
 	return args.Error(0)
 }
