100a098542a340328b092e9bbee46c544f2d24a0
unknown <kauebertaze2004@gmail.com>
2026-08-14 19:00:42 -0300

    fix: general fix

---
+++ src/mocks/repositories/favorite_repository_mock.go
@@ -0,0 +1,17 @@
+package repository_mock
+
+import (
+	"context"
+
+	models "github.com/KaueTTS/streaming_api/src/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type FavoriteRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *FavoriteRepositoryMock) FindByProfileID(ctx context.Context, userID, profileID uint, page, perPage int) ([]models.Favorite, int64, error) {
+	args := m.Called(ctx, userID, profileID, page, perPage)
+	return args.Get(0).([]models.Favorite), args.Get(1).(int64), args.Error(2)
+}
