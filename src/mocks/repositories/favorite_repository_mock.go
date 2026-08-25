91e6eaf102ce6fe83447dbfe5b13d0c6bd6eaaf1
unknown <kauebertaze2004@gmail.com>
2026-08-24 22:33:05 -0300

    test: mocks for the favorites

--- src/mocks/repositories/favorite_repository_mock.go
+++ src/mocks/repositories/favorite_repository_mock.go
@@ -3,6 +3,7 @@ package repository_mock
 import (
 	"context"
 
+	dto_favorite "github.com/KaueTTS/streaming_api/src/api/v1/dto/favorite"
 	models "github.com/KaueTTS/streaming_api/src/models"
 	"github.com/stretchr/testify/mock"
 )
@@ -14,4 +15,14 @@ type FavoriteRepositoryMock struct {
 func (m *FavoriteRepositoryMock) FindFavoriteByProfileID(ctx context.Context, userID, profileID uint, page, perPage int) ([]models.Favorite, int64, error) {
 	args := m.Called(ctx, userID, profileID, page, perPage)
 	return args.Get(0).([]models.Favorite), args.Get(1).(int64), args.Error(2)
+}
+
+func (m *FavoriteRepositoryMock) CreateFavoriteByProfileID(ctx context.Context, userID uint, request dto_favorite.FavoriteRequestDto) error {
+	args := m.Called(ctx, userID, request)
+	return args.Error(0)
+}
+
+func (m *FavoriteRepositoryMock) DeleteFavoriteByProfileID(ctx context.Context, userID uint, request dto_favorite.FavoriteRequestDto) error {
+	args := m.Called(ctx, userID, request)
+	return args.Error(0)
 }
--- src/mocks/services/favorite_service_mock.go
+++ src/mocks/services/favorite_service_mock.go
@@ -3,14 +3,25 @@ package service_mock
 import (
 	"context"
 
+	dto_favorite "github.com/KaueTTS/streaming_api/src/api/v1/dto/favorite"
 	"github.com/stretchr/testify/mock"
 )
 
-type SummaryServiceMock struct {
+type FavoriteServiceMock struct {
 	mock.Mock
 }
 
-func (m *ProfileServiceMock) ListFavorites(ctx context.Context, userID, profileID uint, page, perPage int) error {
+func (m *FavoriteServiceMock) ListFavorites(ctx context.Context, userID, profileID uint, page, perPage int) error {
 	args := m.Called(ctx, userID, profileID, page, perPage)
+	return args.Error(0)
+}
+
+func (m *FavoriteServiceMock) AddFavorite(ctx context.Context, userID uint, request dto_favorite.FavoriteRequestDto) error {
+	args := m.Called(ctx, userID, request)
+	return args.Error(0)
+}
+
+func (m *FavoriteServiceMock) DeleteFavorite(ctx context.Context, userID uint, request dto_favorite.FavoriteRequestDto) error {
+	args := m.Called(ctx, userID, request)
 	return args.Error(0)
 }
